94b601f273b8fd4cf411b4ff2c7e27e333a7eb2b
Egor Belov <georgy.belov@revolut.com>
2019-08-27 18:29:58 +0300

    init save changes

--- xml_visualizer/src/components/ChosenNodeViewer.js
+++ xml_visualizer/src/components/ChosenNodeViewer.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
 import { Box, Flex } from '@rebass/grid'
 import * as actions from 'data/actions'
 
-const Child = styled(Flex)`
+const NodeCard = styled(Flex)`
   align-items: center;
   justify-content: space-between;
   border: 1px solid;
@@ -24,44 +24,63 @@ const getCommonEdges = (node1, node2, network) => {
   return edges1.filter((value) => edges2.includes(value))
 }
 
-const NodeViewer = ({ node, graph, dispatch }) => {
+const NodeItem = (props) => {
+  const dispatch = useDispatch()
+  const graph = useSelector((state) => state.graph.model)
+  const selectedNode = useSelector((state) => state.node)
+
+  const { nodeItem } = props
+  return (
+    <NodeCard
+      p='0.25rem'
+      onMouseOver={() => {
+        graph.network.setSelection(
+          {
+            edges: getCommonEdges(selectedNode, nodeItem, graph.network),
+            nodes: [nodeItem.id, selectedNode.id],
+          },
+          { unselectAll: true, highlightEdges: false }
+        )
+      }}
+      onMouseOut={() => {
+        graph.network.selectNodes([selectedNode.id], false)
+      }}
+    >
+      {nodeItem.nodeName}
+      <div>
+        <button
+          onClick={() => {
+            graph.network.selectNodes([nodeItem.id], true)
+            dispatch(actions.selectNode(nodeItem.id))
+          }}
+        >
+          Выбрать
+        </button>
+      </div>
+    </NodeCard>
+  )
+}
+
+export const ChosenNodeViewer = () => {
+  const node = useSelector((state) => state.node)
+  const [editedValue, setEditedValue] = useState(null)
   const { element } = node
   if (!element) {
     return null
   }
-
-  const renderChild = (childNode, i) => {
-    return (
-      <Child
-        key={i}
-        p='0.25rem'
-        onMouseOver={() => {
-          graph.network.setSelection(
-            {
-              edges: getCommonEdges(node, childNode, graph.network),
-              nodes: [childNode.id, node.id],
-            },
-            { unselectAll: true, highlightEdges: false }
-          )
-        }}
-        onMouseOut={() => {
-          graph.network.selectNodes([node.id], false)
-        }}
-      >
-        {childNode.nodeName}
-        <div>
-          <button
-            onClick={() => {
-              graph.network.selectNodes([childNode.id], true)
-              dispatch(actions.selectNode(childNode.id))
-            }}
-          >
-            Выбрать
-          </button>
-        </div>
-      </Child>
-    )
+  const onEdit = (e) => {
+    setEditedValue(e.target.value)
   }
+  const saveChanges = () => {
+    console.log('saving...', editedValue)
+  }
+  const hasTextNodes = element.children.length < element.childNodes.length
+  const hasChildNodes = element.children.length > 0
+  const defaultValue =
+    hasChildNodes
+      ? element.outerHTML.replace(element.innerHTML || '', '...')
+      : element.outerHTML
+
   return (
     <Box>
       <H4>Элемент:</H4>
@@ -75,20 +94,26 @@ const NodeViewer = ({ node, graph, dispatch }) => {
         <b>Глубина:</b> {element.level}
       </Box>
       <H4>Outer HTML:</H4>
-      <textarea key={element.id} rows={4} defaultValue={element.outerHTML} style={{width: '100%'}}></textarea>
+      <textarea
+        key={element.id}
+        rows={4}
+        defaultValue={defaultValue}
+        style={{ width: '100%' }}
+        onChange={onEdit}
+      ></textarea>
+      {editedValue && (
+        <button onClick={saveChanges}>Сохранить изменения</button>
+      )}
       <H4>Родитель:</H4>
-      {renderChild(element.parentNode)}
+      <NodeItem nodeItem={element.parentNode} />
       <H4>Связанные элементы:</H4>
-      {element.children.length === 0 &&
-        element.childNodes.length !== 0 &&
-        `"${element.childNodes[0].nodeValue}"`}
-      {[...element.children].map(renderChild)}
-
+      {hasTextNodes && !hasChildNodes &&
+        [...element.childNodes].map((textNode, i) => (
+          <div key={i}>{`"${textNode.nodeValue}"`}</div>
+        ))}
+      {[...element.children].map((nodeItem, i) => (
+        <NodeItem nodeItem={nodeItem} key={i} />
+      ))}
     </Box>
   )
 }
-
-export const ChosenNodeViewer = connect((state) => ({
-  node: state.node,
-  graph: state.graph.model,
-}))(NodeViewer)
